40289cf947f00c191f63a9dd1cc573cfe3e568de
Quirin <quirin.ecker@bajupa.com>
2020-08-12 05:07:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const token = require('./config.json').token
+
+client.login(token)
+    .catch(console.log)
+
+client.on('ready', () => {
+    console.log('connected')
+})
+
+client.on('guildMemberRemove', member => {
+    member.guild.systemChannel.send(`<@${member.id}> just left the server`)
+        .then(console.log)
+})
+
+client.on('guildMemberAdd', member => {
+    member.guild.systemChannel.send(`<@${member.id}> just joined the server`)
+        .then(console.log)
+})
